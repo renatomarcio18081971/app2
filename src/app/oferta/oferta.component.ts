0c6a472b8097770219444f35207be5d32043d9b9
renatomarcio18081971 <renato.marcio@gmail.com>
2021-02-10 19:40:15 -0300

    inclusao de componentes filhos

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
+
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public oferta?: Oferta | undefined
+  
+  constructor(
+                private route: ActivatedRoute, 
+                private ofertaService: OfertasService
+             ) { 
+  }
 
   ngOnInit(): void {
+      let id = this.route.snapshot.params['id']
+      if (id !== undefined){
+          this.oferta = this.ofertaService.obterOfertaPorId(id)
+      }
   }
 
 }
