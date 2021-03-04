47b1f139f42d975908df1e1adc669e981de3b2b9
renatomarcio18081971 <renato.marcio@gmail.com>
2021-03-04 08:51:24 -0300

    switchMap e debounce

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,7 +1,11 @@
 import { Observable } from 'rxjs'
 import { Oferta } from './shared/oferta.model'
 import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+
+import { of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, retry, catchError } from 'rxjs/operators';
 
 
 @Injectable()
@@ -28,7 +32,21 @@ export class OfertasService {
     }
 
     public pesquisaOfertas(valor: string): Observable<Oferta[]> {
-        let pesquisa = this.API.concat(valor.toString(), "/descricaooferta")
+        let pesquisa = this.API.concat('ofertas/', valor.toString(), "/descricaooferta")
         return this.http.get<Oferta[]>(pesquisa);
     }
+
+    public apiData = ajax(this.API.concat("ofertas/restaurante/descricaooferta"))
+    .pipe(
+        map((res: any) => {
+            debugger    
+          if (!res.response) {
+            console.log('Error occurred.');
+            throw new Error('Value expected!');
+          }
+          return res.response;
+        }),
+        retry(3), // Retry up to 3 times before failing
+        catchError(err => of([]))
+      );
 }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { OfertasService } from '../ofertas.service';
-import { Oferta } from '../shared/oferta.model';
+import { Component, OnInit } from '@angular/core'
+import { interval, Observable, Subject } from 'rxjs'
+import { OfertasService } from '../ofertas.service'
+import { Oferta } from '../shared/oferta.model'
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/debounceTime'
+
+
+import { mergeMap, switchMap, retry, 
+  map, catchError, filter, scan, debounceTime } from 'rxjs/operators';
+import { debug } from 'console'
 
 @Component({
   selector: 'app-topo',
@@ -13,19 +20,49 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertaService: OfertasService) { }
 
   ngOnInit(): void {
+    // this.ofertas = this.subjectPesquisa.pipe(
+    //     switchMap((consulta: string) => {
+    //     return this.ofertaService.pesquisaOfertas(consulta)
+    // }))
+    // this.ofertas.subscribe((ofertas: Oferta[]) => {
+    //   console.log(ofertas)
+    // })
+
+    this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000),
+      switchMap((consulta: string) => {
+      if(consulta.trim() === ''){
+         return new Observable<Oferta[]>()
+      }  
+      return this.ofertaService.pesquisaOfertas(consulta)
+  }))
+  this.ofertas.subscribe((ofertas: Oferta[]) => {
+    console.log(ofertas)
+  })    
   }
 
   public pesquisa(valorDoCampoPesquisa: string): void {
-      debugger;
-      let retorno = this.ofertaService.pesquisaOfertas(valorDoCampoPesquisa)
-                                      .subscribe(
-                                                    (resultado: Oferta[]) => { console.log(resultado) },
-                                                    (erro) => { console.log(erro) }
-                                            )
-      console.log(retorno)
+    console.log('keyup caracter: ', valorDoCampoPesquisa)
+    this.subjectPesquisa.next(valorDoCampoPesquisa);
+    // if(valorDoCampoPesquisa !== '' && valorDoCampoPesquisa !== undefined){
+    //     let retorno = this.ofertaService.pesquisaOfertas(valorDoCampoPesquisa)
+    //                                     .subscribe(
+    //                                                 resultado => { 
+    //                                                     if(resultado !== undefined){
+    //                                                       console.log(resultado) 
+    //                                                     }
+    //                                                     else {
+    //                                                       console.log('não encontroou registros')
+    //                                                     }
+    //                                                   },
+    //                                                   (erro) => { console.log(' Erro status : '.concat(erro.status)) }
+    //                                           )
+    //     console.log(retorno)
+    //   }
   }
 }
